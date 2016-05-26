3dd0389cdab3aad5553a44593f92e215b9d711e6
tharon-c <design@tharoncarlson.com>
2016-05-27 04:04:25 +0800

    basic file structure and build system

---
+++ src/Button.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+export default react.createClass({
+    render() {
+        return (
+            <button style={{background: "red"}}>
+                Button
+            </button>
+        )
+    }
+}):
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    context: __dirname + "/src",
+    entry: { 
+        javascript: "./index.js",
+        html: "./index.html"
+    },
+
+    output: {
+        filename: "index.js",
+        path: __dirname + "/dist"
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loaders:["react-hot", "babel-loader?presets[]=react,presets[]=es2015"]
+            }
+        ]
+    }
+};
