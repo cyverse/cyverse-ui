0c3fd94ce24980233e282438cd829468d8caee82
tharon-c <design@tharoncarlson.com>
2016-05-28 14:06:22 +0800

    wip testing

---
+++ dist/Button.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _react2.default.createClass({
+    render: function render() {
+
+        return React.createElement(
+            "button",
+            { style: { background: "red" } },
+            "Button"
+        );
+    }
+});
