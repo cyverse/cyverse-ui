c461c95583004f10824ecc544e4834ffa2754316
tharon-c <design@tharoncarlson.com>
2016-07-13 12:09:35 -0700

    Include Icons in lib

---
+++ src/icons/index.js
@@ -0,0 +1,8 @@
+export { default as GithubIcon } from './GithubIcon';
+export { default as WarningIcon } from './WarningIcon';
+export { default as CyverseLogo } from './CyverseLogo';
+export { default as VerticalMenuIcon } from './VerticalMenuIcon';
+export { default as PersonIcon } from './PersonIcon';
+export { default as PlayIcon } from './PlayIcon';
+export { default as ReplayIcon } from './ReplayIcon';
+export { default as CloseIcon } from './CloseIcon';
