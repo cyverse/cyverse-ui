a042c4a56000812526f125a4ff1bd0181ffac961
tharon-c <design@tharoncarlson.com>
2016-07-13 13:24:20 -0700

    Add bottom margin to Tag in TagGroup
    This refactors the marg style to match more than one prop and spread
    into  single returned obj so that combinations of margin can be applied.
    For example, mb={ 2 } mr={ 3 }.

--- src/styles/marg.js
+++ src/styles/marg.js
@@ -5,61 +5,95 @@ const sizes = variables.l.margSizes;
 
 const marg = ( props ) => {
 
+    let style = {};
+
     if (props.mr) {
         if (props.mr.substring) {
-            return {
-                marginRight: props.mr,
+            style = {
+                ...style,
+                ...{
+                    marginRight: props.mr,
+                }
+            };
+        }
+        style = {
+            ...style,
+            ...{
+                marginRight: `${sizes[props.mr - 1]}px`
             }
-        }
-        return {
-            marginRight: `${sizes[props.mr - 1]}px`
-        }
+        };
     }
 
     if (props.mb) {
         if (props.mb.substring) {
-            return {
-                marginBottom: props.mb,
+            style = {
+                ...style,
+                ...{
+                    marginBottom: props.mb,
+                }
+            };
+        }
+        style = {
+            ...style,
+            ...{
+                marginBottom: `${sizes[props.mb -1]}px`
             }
-        }
-        return {
-            marginBottom: `${sizes[props.mb -1]}px`
-        }
+        };
     }
 
     if (props.ml) {
         if (props.ml.substring) {
-            return {
-                marginLeft: props.ml,
+            style = {
+                ...style,
+                ...{
+                    marginLeft: props.ml,
+                }
+            };
+        }
+        style = {
+            ...style,
+            ...{
+                marginLeft: `${sizes[props.ml -1]}px`
             }
-        }
-        return {
-            marginLeft: `${sizes[props.ml -1]}px`
-        }
+        };
     }
 
     if (props.mt) {
         if (props.mt.substring) {
-            return {
-                marginTop: props.mt,
+            style = {
+                ...style, 
+                ...{
+                    marginTop: props.mt,
+                }
+            };
+        }
+        style = {
+            ...style,
+            ...{
+                marginTop: `${sizes[props.mt -1]}px`
             }
-        }
-        return {
-            marginTop: `${sizes[props.mt -1]}px`
-        }
+        };
     }
 
     if (props.ms) {
         if (props.ms.substring) {
-            return {
-                marginRight: props.ms,
+            style = {
+                ...style,
+                ...{
+                    marginRight: props.ms,
+                }
+            };
+        }
+        style = {
+            ...style,
+            ...{
+                marginRight: `${sizes[props.ms -1] * .5}px`,
+                marginLeft: `${sizes[props.ms -1] * .5}px`
             }
-        }
-        return {
-            marginRight: `${sizes[props.ms -1] * .5}px`,
-            marginLeft: `${sizes[props.ms -1] * .5}px`
-        }
+        };
     }
+
+    return style
 }
 
 export default marg;
