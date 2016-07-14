e90c632f500f9912cb655b597a77f59edf0612e8
tharon-c <design@tharoncarlson.com>
2016-07-13 17:01:58 -0700

    Refactor marg for cleaner syntax

--- src/styles/marg.js
+++ src/styles/marg.js
@@ -5,95 +5,43 @@ const sizes = variables.l.margSizes;
 
 const marg = ( props ) => {
 
-    let style = {};
+    //
+    // For more information on how to use this style util 
+    // see troposphere-ui/src/style/README.md 
+    //
 
-    if (props.mr) {
-        if (props.mr.substring) {
-            style = {
-                ...style,
-                ...{
-                    marginRight: props.mr,
-                }
-            };
+    return Object.keys(props).reduce((style, prop) => {
+        let value = props[prop];
+
+        let cssField;
+        switch (prop) {
+            case "mr":
+                style.marginRight = typeof value == "number"
+                    ? sizes[value - 1]
+                    : value
+                break;
+            case "mb":
+                style.marginBottom = typeof value == "number"
+                    ? sizes[value - 1]
+                    : value
+                break;
+            case "ml":
+                style.marginLeft = typeof value == "number"
+                    ? sizes[value - 1]
+                    : value
+                break;
+            case "ms":
+                style.marginRight = typeof value == "number"
+                    ? sizes[value -1] * .5
+                    : value
+                style.marginLeft = typeof value == "number"
+                    ? sizes[props.ms -1] * .5
+                    : value
         }
-        style = {
-            ...style,
-            ...{
-                marginRight: `${sizes[props.mr - 1]}px`
-            }
-        };
-    }
 
-    if (props.mb) {
-        if (props.mb.substring) {
-            style = {
-                ...style,
-                ...{
-                    marginBottom: props.mb,
-                }
-            };
-        }
-        style = {
-            ...style,
-            ...{
-                marginBottom: `${sizes[props.mb -1]}px`
-            }
-        };
-    }
+        return style
 
-    if (props.ml) {
-        if (props.ml.substring) {
-            style = {
-                ...style,
-                ...{
-                    marginLeft: props.ml,
-                }
-            };
-        }
-        style = {
-            ...style,
-            ...{
-                marginLeft: `${sizes[props.ml -1]}px`
-            }
-        };
-    }
-
-    if (props.mt) {
-        if (props.mt.substring) {
-            style = {
-                ...style, 
-                ...{
-                    marginTop: props.mt,
-                }
-            };
-        }
-        style = {
-            ...style,
-            ...{
-                marginTop: `${sizes[props.mt -1]}px`
-            }
-        };
-    }
-
-    if (props.ms) {
-        if (props.ms.substring) {
-            style = {
-                ...style,
-                ...{
-                    marginRight: props.ms,
-                }
-            };
-        }
-        style = {
-            ...style,
-            ...{
-                marginRight: `${sizes[props.ms -1] * .5}px`,
-                marginLeft: `${sizes[props.ms -1] * .5}px`
-            }
-        };
-    }
-
-    return style
+    }, {});
 }
 
 export default marg;
