7482550bf62b3b5c097bb3cefc88a7ad00cdca2a
tharon-c <design@tharoncarlson.com>
2016-09-06 15:44:24 -0700

    Add new Icons to index.js

--- src/icons/index.js
+++ src/icons/index.js
@@ -5,3 +5,5 @@ export { default as PersonIcon } from './PersonIcon';
 export { default as PlayIcon } from './PlayIcon';
 export { default as ReplayIcon } from './ReplayIcon';
 export { default as CloseIcon } from './CloseIcon';
+export { default as ConsoleIcon } from './ConsoleIcon';
+export { default as PauseIcon } from './PauseIcon';
