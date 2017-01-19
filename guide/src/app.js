26477504710261e0e63ba49e1adfe00e5ba53ee8
tharon-c <design@tharoncarlson.com>
2017-01-19 11:14:58 -0700

    Port material-ui SelectField to SelectMenu

--- guide/src/app.js
+++ guide/src/app.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import React from 'react';
-import StyleGuide from "./StyleGuide.js";
+import StyleGuide from './StyleGuide.js';
 
 ReactDOM.render(
     <StyleGuide/>,
--- guide/src/examples/SelectMenuEx.js
+++ guide/src/examples/SelectMenuEx.js
@@ -6,36 +6,46 @@ import theme from '../theme.js';
 export default React.createClass({
     getInitialState() {
         return {
-            selected: 0,
+            selected: "2",
         }
     },
 
     things: [
         {
+            id: "1",
             name: "Red Fish",
             color: "Red",
             home: "Ocean",
         },
         {
+            id: "2",
             name: "Blue Fish",
             color: "Blue",
             home: "Lake",
         },
         {
+            id: "3",
             name: "Green Fish",
             color: "Green",
             home: "Pond"
         }
     ],
 
-    optionName(i) {
-        return this.things[i].name
-    },
-
-    onSelectChange(selected) {
+    onSelectChange(e, i, selected) {
+        console.log(selected);
         this.setState({
             selected
         });
+    },
+
+    menuItem({ id, name }) { 
+        return (
+            <MenuItem
+                value={ id }
+                primaryText={ name }
+                color={ theme.color.primary }
+            />
+        )
     },
 
     render() {
@@ -48,14 +58,8 @@ export default React.createClass({
                             maxWidth: "300px",
                         }}
                     >
-                        <SelectMenu
-                            list={ this.things }
-                            optionName={ this.optionName }
-                            color={ theme.color.primary }
-                            selected={ this.state.selected }
-                            onSelectChange={ this.onSelectChange }
-                        />
-                    </div>
+                        { this.things.map(this.menuItem) }
+                    </SelectMenu>
                 </Sheet>
                 <Code children={
                     /* This is a string for our code snippt. It is not indented because it messes up the formating in render
