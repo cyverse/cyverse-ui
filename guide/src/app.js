a93aacee94b2db43de56f39681491964b9554d84
Connor Osborn <cdosborn@email.arizona.edu>
2017-01-19 19:00:46 -0700

    Move warning before render call

--- guide/src/app.js
+++ guide/src/app.js
@@ -2,6 +2,13 @@ import ReactDOM from "react-dom";
 import React from 'react';
 import StyleGuide from './StyleGuide.js';
 
+// These two lines are necessary to interop with material-ui. It's supposed to
+// be temporary. When the installation no longer mentions it, I suppose these
+// can be removed.
+// http://www.material-ui.com/#/get-started/installation
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 ReactDOM.render(
     <StyleGuide/>,
     document.getElementById('app')
