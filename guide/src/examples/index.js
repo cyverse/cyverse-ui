5c0c2ee0bbcac5591d87b7a2250157543702c1cd
tharon-c <design@tharoncarlson.com>
2017-02-06 16:01:38 -0700

    Remove material-ui conflict

--- guide/src/examples/index.js
+++ guide/src/examples/index.js
@@ -3,7 +3,6 @@ export { default as AvatarEx } from './AvatarEx';
 export { default as TabsEx } from './TabsEx';
 export { default as MeterGaugeEx } from './MeterGaugeEx';
 export { default as ShowMoreEllipsisEx } from './ShowMoreEllipsisEx';
-export { default as CircleProgressBarEx } from './CircleProgressBarEx';
 export { default as PillEx } from './PillEx';
 export { default as MediaCardGroupEx } from './MediaCardGroupEx'
 export { default as InstanceCardEx } from './InstanceCardEx'
