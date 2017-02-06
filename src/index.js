5d7f8b92db758ae0ccc5f815d63dcf88e1e9cb71
tharon-c <design@tharoncarlson.com>
2017-02-06 16:00:27 -0700

    Change Badge to Pill

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export { default as Button } from './Button';
 export { default as FlatButton } from './FlatButton';
 export { default as Avatar } from './Avatar';
-export { default as Badge } from './Badge';
+export { default as Pill } from './Pill';
 export { default as Tag } from './Tag';
 export { default as CircleProgressBar } from './CircleProgressBar';
 export { default as Dialog } from './Dialog';
