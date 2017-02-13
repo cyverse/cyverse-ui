eb667797c925dddf95c7c7be16853905e81975c0
tharon-c <design@tharoncarlson.com>
2017-02-13 14:02:33 -0700

    SubHeader MUI over CUI, improve example

--- guide/src/examples/SubHeaderEx.js
+++ guide/src/examples/SubHeaderEx.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import Share from 'material-ui/svg-icons/social/Share';
+import Delete from 'material-ui/svg-icons/action/Delete';
+import Edit from 'material-ui/svg-icons/image/Edit';
 import { SubHeader, Div, Title } from 'cyverse-ui';
 import { Code }  from '../components';
-import { PersonIcon } from '../icons';
+import { DeleteIcon, EditIcon, ConsoleIcon } from 'cyverse-ui/icons';
 import Paper from 'material-ui/Paper';
-
 
 export default React.createClass({
     render() {
         return (
             <section>
-                <Paper style={{ padding: "0 10px", marginBottom: "20px"}} >
+                <Paper style={{ padding: "5px", marginBottom: "20px"}} >
                     <SubHeader 
-                        name="Title of View" 
+                        name="Title of View"
+                        menuItems={[
+                            <MenuItem key="1" primaryText="Refresh" />,
+                            <MenuItem key="2" primaryText="Send feedback" />,
+                            <MenuItem key="3" primaryText="Settings" />,
+                            <MenuItem key="4" primaryText="Help" />,
+                        ]}
+
+                    />
+                </Paper>
+                <Paper style={{ padding: "5px", marginBottom: "20px"}} >
+                    <SubHeader
+                        name="With QuickActions"
+                        quickActions={[
+                            <IconButton key="1">
+                                <Share/>
+                            </IconButton>,
+                            <IconButton key="1">
+                                <Edit size={ 20 }/>
+                            </IconButton>,
+                            <IconButton key="1">
+                                <Delete size={ 20 }/>
+                            </IconButton>,
+
+                        ]}
                         menuItems={[
                             <MenuItem key="1" primaryText="Refresh" />,
                             <MenuItem key="2" primaryText="Send feedback" />,
@@ -24,9 +51,33 @@ export default React.createClass({
                 </Paper>
             <Code
                 children={
-`<Paper style={{ padding: "0 10px", marginBottom: "20px"}} >
+`<Paper style={{ padding: "5px", marginBottom: "20px"}} >
     <SubHeader 
-        name="Title of View" 
+        name="Title of View"
+        menuItems={[
+            <MenuItem key="1" primaryText="Refresh" />,
+            <MenuItem key="2" primaryText="Send feedback" />,
+            <MenuItem key="3" primaryText="Settings" />,
+            <MenuItem key="4" primaryText="Help" />,
+        ]}
+
+    />
+</Paper>
+<Paper style={{ padding: "5px", marginBottom: "20px"}} >
+    <SubHeader 
+        name="With QuickActions"
+        quickActions={[
+            <IconButton key="1">
+                <Share/>
+            </IconButton>,
+            <IconButton key="1">
+                <Edit size={ 20 }/>
+            </IconButton>,
+            <IconButton key="1">
+                <Delete size={ 20 }/>
+            </IconButton>,
+
+        ]}
         menuItems={[
             <MenuItem key="1" primaryText="Refresh" />,
             <MenuItem key="2" primaryText="Send feedback" />,
