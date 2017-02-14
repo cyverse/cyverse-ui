850a44f5933daaa9895564d5bd3d9f61da69fec9
tharon-c <design@tharoncarlson.com>
2017-02-14 13:56:11 -0700

    style check / Avatar on MediaCard

--- guide/src/theme.js
+++ guide/src/theme.js
@@ -3,6 +3,11 @@ const theme = {
         primary: "#0971ab",
         danger: "#ea5050",
         success: "#5cb85c",
+    },
+    palette: {
+        primary1Color: "#0971ab",
+        danger: "#ff470a",
+        success: "#5cb85c",
     }
 }
 
