95a698d5d3e0316528bc014616e615266c82b748
tharon-c <design@tharoncarlson.com>
2017-03-20 12:08:03 -0700

    change theme colors to hex

--- src/makeTheme.js
+++ src/makeTheme.js
@@ -4,22 +4,31 @@ var requireMany = require('require-many');
 var cm = require('material-ui/utils/colorManipulator');
 var colors = require('material-ui/styles/colors');
 
+var primary1Color = "#0971ab",
+    primary2Color = "#318cc8",
+    accent1Color = "#57cbea";
+
 var theme = {
-  palette: {
-    primary1Color: "#0971ab",
-    primary2Color: "rgb(49, 140, 200)",
-    primary3Color: colors.grey400,
-    accent1Color: "rgba(87, 203, 234, 1)",
-    accent2Color: colors.grey100,
-    accent3Color: colors.grey500,
-    alternateTextColor: colors.white,
-    danger: "rgb(255, 47, 13)",
-    success: "#5cb85c",
-    canvasColor: colors.white,
-    borderColor: colors.grey300,
-    pickerHeaderColor: "rgb(49, 140, 200)",
-    shadowColor: colors.fullBlack,
-  },
+    header: {
+        headerColor: primary1Color,
+        headerBorderColor: accent1Color,
+        headerLinkColor: "rgba(255, 255, 255, 0.7)"
+    },
+    palette: {
+        primary1Color: primary1Color,
+        primary2Color: primary2Color,
+        primary3Color: colors.grey400,
+        accent1Color: accent1Color,
+        accent2Color: colors.grey100,
+        accent3Color: colors.grey500,
+        alternateTextColor: colors.white,
+        danger: "#ff2f0d",
+        success: "#5cb85c",
+        canvasColor: colors.white,
+        borderColor: colors.grey300,
+        pickerHeaderColor: primary2Color,
+        shadowColor: colors.fullBlack,
+    },
 };
 
 writeJson('./styles/cyverseTheme.json', theme, function(err) {
