336d064f7ac496bea4db51f05f8a09994303a0dd
Tharon Carlson <design@tharoncarlson.com>
2017-06-12 22:17:39 +0000

    Remove use of react-dimensions
    Causes bug where only 0s are rendered if height not found
    This is problematic in some cases and not worth the work around.

--- src/MediaCard.js
+++ src/MediaCard.js
@@ -233,10 +233,7 @@ const MediaCard = React.createClass({
             opacity: "1",
             ...styles.t.body1,
         };
-        if (this.props.containerWidth <= 750) {
-            style.summary.marginTop = "30px";
-            style.summary.marginRight = "0px";
-        }
+
         if (this.props.isExpanded) {
             style.summary.display = "none";
         }
@@ -270,6 +267,4 @@ const MediaCard = React.createClass({
     }
 });
 
-export default Dimensions({ 
-    containerStyle: { height: 'auto' }
-})(MediaCard);
+export default MediaCard
