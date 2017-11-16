84c2523b9f4fb7487891a2a799b21394c9e651c6
tharon-c <design@tharoncarlson.com>
2017-11-16 14:33:16 -0700

    pin version, enable linking to component by hash

--- guide/src/components/ComponentLinkList.js
+++ guide/src/components/ComponentLinkList.js
@@ -1,45 +1,51 @@
-import React from 'react';
-import R from 'ramda';
-import Scroll from 'react-scroll';
+import React from "react";
+import R from "ramda";
+import Scroll from "react-scroll";
 
-import {List, ListItem} from 'material-ui/List';
+import { List, ListItem } from "material-ui/List";
 
-import * as componentDocs from '../componentDocs';
+import * as componentDocs from "../componentDocs";
 
 const scroller = Scroll.scroller;
 const scrollTo = target => {
-        return () => {
-            scroller.scrollTo(target, {
-                duration: 1000,
-                smooth: true,
-            });
-        }
+    return () => {
+        scroller.scrollTo(target, {
+            duration: 1000,
+            smooth: true
+        });
     };
+};
 
 class ComponentLinkList extends React.Component {
     renderComponentLinks = () => {
-        return R.toPairs(componentDocs).map( item => {
+        return R.toPairs(componentDocs).map(item => {
             // This is kind of lame because the file ends with "doc"
             // Probably a better way to get the name off the component
             const name = item[0].slice(0, -3);
+            const target = name
+                .replace(/(^[A-Z])/, ([first]) => first.toLowerCase())
+                .replace(
+                    /([A-Z])/g,
+                    ([letter]) => `-${letter.toLowerCase()}`
+                );
             return (
-            <ListItem
-                onTouchTap={ scrollTo(name) }
-                primaryText= { name }
-            />
-            )
-        })
+                <ListItem
+                    onTouchTap={scrollTo(target)}
+                    primaryText={name}
+                />
+            );
+        });
     };
 
     render() {
         return (
             <ListItem
                 primaryTogglesNestedList={true}
-                nestedItems={ this.renderComponentLinks() }
+                nestedItems={this.renderComponentLinks()}
             >
                 Components
             </ListItem>
-        )
+        );
     }
 }
 
