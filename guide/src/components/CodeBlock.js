7dd9989758bb5a29117cf3a5749b0ddd3cfcf7c0
tharon-c <design@tharoncarlson.com>
2018-05-16 11:23:42 -0700

    Collapse code examples

--- guide/src/components/CodeBlock.js
+++ guide/src/components/CodeBlock.js
@@ -1,24 +1,33 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import marked from 'marked';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import marked from "marked";
+import { Collapse } from "material-ui/transitions";
+import { Button } from "material-ui";
+import { Element } from "cyverse-ui";
 
 const styles = {
     root: {
         marginTop: 20,
         marginBottom: 20,
-        padding: '0 10px',
+        padding: "0 10px",
     },
 };
 
 class CodeBlock extends Component {
-
+    state = { open: false };
     static propTypes = {
         style: PropTypes.object,
         text: PropTypes.string.isRequired,
     };
 
     static defaultProps = {
-        text: '',
+        text: "",
+    };
+
+    handleOpen = () => {
+        this.setState({
+            open: !this.state.open,
+        });
     };
 
     componentWillMount() {
@@ -30,32 +39,37 @@ class CodeBlock extends Component {
             sanitize: false,
             smartLists: true,
             smartypants: false,
-            language: 'jsx',
-            highlight: (code, lang) => (
-                require('highlight.js').highlight(lang, code).value
-            ),
+            language: "jsx",
+            highlight: (code, lang) =>
+                require("highlight.js").highlight(lang, code).value,
         });
     }
 
     render() {
-        const {
-            style,
-            text,
-        } = this.props;
+        const { style, text } = this.props;
+        const { open } = this.state;
 
-        const renderCode = (
-`\`\`\`jsx
+        const renderCode = `\`\`\`jsx
 ${text}
-\`\`\``
-        );
+\`\`\``;
 
         /* eslint-disable react/no-danger */
         return (
-            <div
-                style={{ ...styles.root, ...style}}
-                className="markdown-body"
-                dangerouslySetInnerHTML={{__html: marked(renderCode)}}
-            />
+            <Element whitespace="pt3">
+                <Button onClick={this.handleOpen}>
+                    {`</> ${open ? "Hide" : "View"} Code Example`}
+                </Button>
+
+                <Collapse in={open}>
+                    <div
+                        style={{ ...styles.root, ...style }}
+                        className="markdown-body"
+                        dangerouslySetInnerHTML={{
+                            __html: marked(renderCode),
+                        }}
+                    />
+                </Collapse>
+            </Element>
         );
         /* eslint-enable */
     }
