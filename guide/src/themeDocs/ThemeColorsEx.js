c7a91f06ff84067e6b66cddbf0d488b04e42a089
tharon-c <design@tharoncarlson.com>
2018-07-30 13:13:39 -0700

    formating

--- guide/src/themeDocs/ThemeColorsEx.js
+++ guide/src/themeDocs/ThemeColorsEx.js
@@ -34,7 +34,7 @@ const styles = theme => {
             height: "200px",
             "& div": {
                 paddingLeft: 8,
-            }
+            },
         },
         swatchLong: {
             height: "500px",
@@ -161,23 +161,23 @@ const styles = theme => {
         },
         actionActive: {
             height: "100%",
-            background: action.active
+            background: action.active,
         },
         actionDisabled: {
             height: "100%",
-            background: action.disabled
+            background: action.disabled,
         },
         actionDisabledBackground: {
             height: "100%",
-            background: action.disabledBackground
+            background: action.disabledBackground,
         },
         actionHover: {
             height: "100%",
-            background: action.hover
+            background: action.hover,
         },
         actionSelected: {
             height: "100%",
-            background: action.selected
+            background: action.selected,
         },
     };
 };
@@ -187,57 +187,97 @@ class ThemeColorsEx extends React.Component {
         return (
             <div className={classes.wrapper}>
                 <Paper className={classes.swatch}>
-                    <div className={classes.primaryMain}>Primary Main</div>
-                    <div className={classes.primaryDark} >Primary Dark</div>
-                    <div className={classes.primaryLight} >Primary Light</div>
+                    <div className={classes.primaryMain}>
+                        Primary Main
+                    </div>
+                    <div className={classes.primaryDark}>
+                        Primary Dark
+                    </div>
+                    <div className={classes.primaryLight}>
+                        Primary Light
+                    </div>
                 </Paper>
                 <Paper className={classes.swatch}>
-                    <div className={classes.secondaryMain} >Secondary Main</div>
-                    <div className={classes.secondaryDark} >Secondary Dark</div>
-                    <div className={classes.secondaryLight} >Secondary Light</div>
+                    <div className={classes.secondaryMain}>
+                        Secondary Main
+                    </div>
+                    <div className={classes.secondaryDark}>
+                        Secondary Dark
+                    </div>
+                    <div className={classes.secondaryLight}>
+                        Secondary Light
+                    </div>
                 </Paper>
                 <Paper className={classes.swatch}>
-                    <div className={classes.errorMain} >Error Main</div>
-                    <div className={classes.errorDark} >Error Dark</div>
-                    <div className={classes.errorLight} >Error Light</div>
+                    <div className={classes.errorMain}>
+                        Error Main
+                    </div>
+                    <div className={classes.errorDark}>
+                        Error Dark
+                    </div>
+                    <div className={classes.errorLight}>
+                        Error Light
+                    </div>
                 </Paper>
                 <Paper className={classes.swatch}>
-                    <div className={classes.backgroundPaper} >Background Paper</div>
-                    <div className={classes.backgroundDefault} >Background Default</div>
+                    <div className={classes.backgroundPaper}>
+                        Background Paper
+                    </div>
+                    <div className={classes.backgroundDefault}>
+                        Background Default
+                    </div>
                 </Paper>
                 <Paper className={classes.swatch}>
-                    <div className={classes.textDisabled} >Text Disabled</div>
-                    <div className={classes.textPrimary} >Text Primary</div>
-                    <div className={classes.textSecondary} >Text Secondary</div>
-                    <div className={classes.textHint} >Text Hint</div>
+                    <div className={classes.textDisabled}>
+                        Text Disabled
+                    </div>
+                    <div className={classes.textPrimary}>
+                        Text Primary
+                    </div>
+                    <div className={classes.textSecondary}>
+                        Text Secondary
+                    </div>
+                    <div className={classes.textHint}>Text Hint</div>
                 </Paper>
                 <Paper className={classes.swatch}>
-                    <div className={classes.actionActive} >Action Active</div>
-                    <div className={classes.actionDisabled} >Action Disabled</div>
-                    <div className={classes.actionDisabledBackground} >Action Disabled Background</div>
-                    <div className={classes.actionHover} >Action Hover</div>
-                    <div className={classes.actionSelected} >Action Selected</div>
+                    <div className={classes.actionActive}>
+                        Action Active
+                    </div>
+                    <div className={classes.actionDisabled}>
+                        Action Disabled
+                    </div>
+                    <div className={classes.actionDisabledBackground}>
+                        Action Disabled Background
+                    </div>
+                    <div className={classes.actionHover}>
+                        Action Hover
+                    </div>
+                    <div className={classes.actionSelected}>
+                        Action Selected
+                    </div>
                 </Paper>
                 <Paper className={classes.swatch}>
-                    <div className={classes.divider} >Divider</div>
+                    <div className={classes.divider}>Divider</div>
                 </Paper>
                 <Paper
-                    className={[classes.swatch, classes.swatchLong]}
+                    className={`${classes.swatch} ${
+                        classes.swatchLong
+                    }`}
                 >
-                    <div className={classes.grey50} >Grey 50</div>
-                    <div className={classes.grey100} >Grey 100</div>
-                    <div className={classes.grey200} >Grey 200</div>
-                    <div className={classes.grey300} >Grey 300</div>
-                    <div className={classes.grey400} >Grey 400</div>
-                    <div className={classes.grey500} >Grey 500</div>
-                    <div className={classes.grey600} >Grey 600</div>
-                    <div className={classes.grey700} >Grey 700</div>
-                    <div className={classes.grey800} >Grey 800</div>
-                    <div className={classes.grey900} >Grey 900</div>
-                    <div className={classes.greyA100} >Grey A100</div>
-                    <div className={classes.greyA200} >Grey A200</div>
-                    <div className={classes.greyA400} >Grey A400</div>
-                    <div className={classes.greyA700} >Grey A700</div>
+                    <div className={classes.grey50}>Grey 50</div>
+                    <div className={classes.grey100}>Grey 100</div>
+                    <div className={classes.grey200}>Grey 200</div>
+                    <div className={classes.grey300}>Grey 300</div>
+                    <div className={classes.grey400}>Grey 400</div>
+                    <div className={classes.grey500}>Grey 500</div>
+                    <div className={classes.grey600}>Grey 600</div>
+                    <div className={classes.grey700}>Grey 700</div>
+                    <div className={classes.grey800}>Grey 800</div>
+                    <div className={classes.grey900}>Grey 900</div>
+                    <div className={classes.greyA100}>Grey A100</div>
+                    <div className={classes.greyA200}>Grey A200</div>
+                    <div className={classes.greyA400}>Grey A400</div>
+                    <div className={classes.greyA700}>Grey A700</div>
                 </Paper>
             </div>
         );
